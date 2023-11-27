687b5547b86dcdef0fc77362b339903273ba06a3
User <user@C02FW4SLMD6P.local>
2023-11-27 17:04:23 +0400

    Initial commit

---
+++ app/src/main/java/com/sweeft/myapp/LoginActivity.kt
@@ -0,0 +1,114 @@
+package com.sweeft.myapp
+
+import android.content.Intent
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.sweeft.myapp.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = ActivityLoginBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            val actionBar = supportActionBar
+
+            if (actionBar != null) {
+                actionBar.setHomeAsUpIndicator(R.drawable.baseline_arrow_back)
+                title = ""
+                supportActionBar?.elevation = 0F
+                supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this, R.color.white)))
+                actionBar.setDisplayHomeAsUpEnabled(true)
+            }
+
+            binding.etLoginPhone.addTextChangedListener(watcher)
+            binding.etPassword.addTextChangedListener(watcher)
+
+            binding.btnLogin.setOnClickListener {
+
+                binding.layoutWarning.visibility=View.VISIBLE
+            }
+            binding.textView2.setOnClickListener {
+                Toast.makeText(this@LoginActivity, "Forgot Password Clicked!", Toast.LENGTH_SHORT).show()
+            }
+            binding.etLoginPhone.filters = arrayOf(InputFilter.LengthFilter(15))
+
+        }
+
+        private val watcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(charSequence: CharSequence, start: Int, before: Int, count: Int) {
+                val isLoginPhoneNotEmpty = binding.etLoginPhone.text?.isNotEmpty() == true
+                val isPasswordNotEmpty = binding.etPassword.text?.isNotEmpty() == true
+
+                binding.btnLogin.isEnabled = isLoginPhoneNotEmpty && isPasswordNotEmpty
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (binding.etLoginPhone.text?.isEmpty()!! || binding.etPassword.text?.isEmpty()!!) {
+                    binding.layoutWarning.visibility = View.GONE
+                }
+                val password = binding.etPassword.text.toString()
+                val phoneNumber = binding.etLoginPhone.text.toString()
+
+                if (binding.etPassword.text!!.isNotEmpty()) {
+                    val passContainsNumber = password.contains("\\d+".toRegex())
+                    val passContainsUppercase = password.any { it.isUpperCase() }
+
+                    if (!passContainsNumber || !passContainsUppercase || password.length < 9) {
+                        binding.etPassword.error = "Invalid password"
+                    }
+                    else{ binding.etLoginPhone.error = null
+                    }
+                }
+
+                if(phoneNumber.length<9){
+                    binding.etLoginPhone.error = "Invalid phone number"
+                }
+                else{ binding.etLoginPhone.error = null
+                }
+            }
+        }
+
+        override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            return when (item.itemId) {
+                android.R.id.home -> {
+                    val intent = Intent(applicationContext, SplashScreenSecond::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                R.id.action_sign_up -> {
+                    Toast.makeText(this@LoginActivity, "Sign up clicked!", Toast.LENGTH_SHORT).show()
+                    /*
+                    val intent = Intent(applicationContext, SignUpActivity::class.java)
+                    startActivity(intent)
+                     */
+                    true
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+        }
+        override fun onCreateOptionsMenu(menu: Menu): Boolean {
+            menuInflater.inflate(R.menu.menu_login, menu)
+
+            val yourMenuItem = menu.findItem(R.id.action_sign_up)
+            yourMenuItem.title = "Sign up"
+
+            return true
+        }
+
+}
---
+++ app/src/main/java/com/sweeft/myapp/SplashScreenSecond.kt
@@ -0,0 +1,71 @@
+package com.sweeft.myapp
+
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.Typeface
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.sweeft.myapp.databinding.ActivitySplashScreenSecondBinding
+
+class SplashScreenSecond : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenSecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenSecondBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnRegister.setOnClickListener {
+            Toast.makeText(this@SplashScreenSecond, "Register clicked!", Toast.LENGTH_SHORT).show()
+
+           // register()
+        }
+
+        termsAndConditionsStyling()
+
+    }
+
+    private fun register() {
+        TODO("Not yet implemented")
+    }
+
+
+
+    private fun termsAndConditionsStyling() {
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                Toast.makeText(this@SplashScreenSecond, "Clicked!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        val spannableString = SpannableString("By selecting one of the options above, you accept the terms and conditions")
+
+        val termsAndConditionsText = "terms and conditions"
+        val startIndex = spannableString.indexOf(termsAndConditionsText)
+        val endIndex = startIndex + termsAndConditionsText.length
+        val color = ContextCompat.getColor(this,R.color.orange1)
+
+     spannableString.setSpan(StyleSpan(Typeface.BOLD), startIndex, endIndex, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
+       spannableString.setSpan(clickableSpan, startIndex, endIndex, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(ForegroundColorSpan(color), startIndex, endIndex, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        binding.tvTermsAndConditions.movementMethod = LinkMovementMethod.getInstance()
+        binding.tvTermsAndConditions.text = spannableString
+
+
+    }
+
+}
